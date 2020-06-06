e52f82adf5a8bbcb7255fbb374894de4172ef67c
hunter-s70 <serg.buhtoyarov@gmail.com>
2020-06-06 18:08:35 +0300

    add url-filtration

--- src/app/components/employees-filters/employees-filters.component.ts
+++ src/app/components/employees-filters/employees-filters.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Output, Input, EventEmitter } from '@angular/core';
 import { SelectItem } from '../../interfaces/select-item';
 import { EmployeeService } from '../../services/employee.service';
+import { EmployeesFilters } from '../../interfaces/employees-filters';
 
 @Component({
   selector: 'app-employees-filters',
@@ -13,13 +14,14 @@ export class EmployeesFiltersComponent implements OnInit {
     private ems: EmployeeService,
   ) { }
 
+  @Input() initFilters: EmployeesFilters;
   @Output() filtersChanged = new EventEmitter<object>();
   @Output() filtersReset = new EventEmitter<object>();
 
-  skill = '';
-  positionId = '';
-  searchText = '';
-  searchBy = '';
+  skill: string;
+  positionId: string;
+  searchText: string;
+  searchBy: string;
   searchByList: SelectItem[] = [
     {id: 'email', value: 'Email'},
     {id: 'lastName', value: 'Surname'},
@@ -65,6 +67,10 @@ export class EmployeesFiltersComponent implements OnInit {
   }
 
   ngOnInit() {
+    this.skill = this.initFilters.skill || '';
+    this.positionId = this.initFilters.positionId || '';
+    this.searchText = this.initFilters.searchText || '';
+    this.searchBy = this.initFilters.searchBy || '';
   }
 
 }
--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -3,6 +3,7 @@ import { AuthService } from '../../services/auth.service';
 import { EmployeeService } from '../../services/employee.service';
 import { Subscription } from 'rxjs';
 import { EmployeesFilters } from '../../interfaces/employees-filters';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard',
@@ -14,32 +15,29 @@ export class DashboardComponent implements OnInit, OnDestroy {
   constructor(
     private ems: EmployeeService,
     public authService: AuthService,
+    private route: ActivatedRoute,
+    public router: Router,
   ) { }
 
-  employeesList = [];
   employeeData: Subscription;
   employeesPerPage: number;
-  lastVisibleDoc: '';
-  isFullList = this.authService.isAdmin;
+  employeesList: any[];
+  lastVisibleDoc: any;
+  initFiltersParams: object;
+  isFullList: boolean;
 
-  private _getAllEmployees(): void {
-    this._checkSubscription();
-    this.employeeData = this.ems
-      .getEmployeesList({limit: this.employeesPerPage}, this.isFullList)
-      .subscribe(({employeesList, lastVisibleDoc}) => {
-        this.employeesList = employeesList;
-        this.lastVisibleDoc = lastVisibleDoc;
-      });
+  get showLoadMoreBtn(): boolean {
+    return !!(this.lastVisibleDoc && !Object.keys(this.initFiltersParams).length);
   }
 
   applyFilters(params: EmployeesFilters): void {
-    this._checkSubscription();
-    this.employeeData = this.ems
-      .getEmployeesList(params, this.isFullList)
-      .subscribe(({employeesList}) => {
-        this.employeesList = employeesList;
-        this.lastVisibleDoc = null;
-      });
+    const queryParams = {};
+    for (const key of Object.keys(params)) {
+      if (params[key]) {
+        queryParams[key] = params[key];
+      }
+    }
+    this.router.navigate(['/app/home'], {queryParams});
   }
 
   resetFilters(): void {
@@ -62,13 +60,43 @@ export class DashboardComponent implements OnInit, OnDestroy {
     }
   }
 
+  private _getAllEmployees(): void {
+    this._checkSubscription();
+    this.employeeData = this.ems
+      .getEmployeesList({limit: this.employeesPerPage}, this.isFullList)
+      .subscribe(({employeesList, lastVisibleDoc}) => {
+        this.employeesList = employeesList;
+        this.lastVisibleDoc = lastVisibleDoc;
+      });
+  }
+
+  private _getFilteredEmployees(params: EmployeesFilters): void {
+    this._checkSubscription();
+    this.employeeData = this.ems
+      .getEmployeesList(params, this.isFullList)
+      .subscribe(({employeesList}) => {
+        this.employeesList = employeesList;
+        this.lastVisibleDoc = null;
+      });
+  }
+
   ngOnInit() {
     this.employeesPerPage = 15;
+    this.employeesList = [];
+    this.lastVisibleDoc = '';
+    this.initFiltersParams = {};
+    this.isFullList = this.authService.isAdmin;
+
     this._getAllEmployees();
+
+    this.route.queryParams.subscribe(params => {
+      this.initFiltersParams = params;
+      this._getFilteredEmployees(params);
+    });
   }
 
   ngOnDestroy() {
-    this.employeeData.unsubscribe();
+    this._checkSubscription();
   }
 
 }
