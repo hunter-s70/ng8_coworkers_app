1ffde77b21cb5d79dbea2688533726cfb3082106
hunter-s70 <serg.buhtoyarov@gmail.com>
2020-06-08 11:26:40 +0300

    show employee page data and markup

--- src/app/pages/show-employee/show-employee.component.ts
+++ src/app/pages/show-employee/show-employee.component.ts
@@ -26,6 +26,50 @@ export class ShowEmployeeComponent implements OnInit, OnDestroy {
     return this.employee ? `${this.employee.firstName} ${this.employee.lastName}` : '';
   }
 
+  get dataList(): any[] {
+    const employeeData = this.employee || {};
+    console.log(this.employee.skillsList.join('; '));
+    return [
+      {
+        title: 'Position',
+        data: this.ems.getPositionNameById(this.employee.positionId),
+      },
+      {
+        title: 'Skills',
+        class: 'show-employee__data--skills',
+        data: this.employee.skillsList.map(item => `<span class="skill-clip">${item}</span>`).join(''),
+      },
+      {
+        title: 'Email',
+        data: this.employee.email,
+      },
+      {
+        title: 'Telegram link',
+        data: this.employee.telegramLink ?
+          `<a href="${this.employee.telegramLink}" target="_blank">${this.employee.telegramLink}</a>` :
+          '',
+      },
+      {
+        title: 'CV link',
+        data: this.employee.cvLink ?
+          `<a href="${this.employee.cvLink}" target="_blank">${this.employee.cvLink}</a>` :
+          '',
+      },
+      {
+        title: 'Birthday',
+        data: this.employee.getBirthday().format('DD/MM/YYYY'),
+      },
+      {
+        title: 'First working day',
+        data: this.employee.getFirstday().format('DD/MM/YYYY'),
+      },
+      {
+        title: 'Biography',
+        data: this.employee.bio,
+      },
+    ];
+  }
+
   private _getEmployeeData(employeeId: string): void {
     this.employeeData = this.ems.getEmployeeById(employeeId).subscribe((data) => {
       this.employeeExists = data.exists;
