1e3c38fcbfa8b1133de068822dbf59a14f6f2c6f
hunter-s70 <serg.buhtoyarov@gmail.com>
2020-06-13 16:29:55 +0300

    add manage skills page

---
+++ src/app/pages/skills-manager/skills-manager.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-skills-manager',
+  templateUrl: './skills-manager.component.html',
+  styleUrls: ['./skills-manager.component.css']
+})
+export class SkillsManagerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
