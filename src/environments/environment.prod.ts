7f7016f22e693863aa19358796c8a5ef7707bd40
hunter-s70 <serg.buhtoyarov@gmail.com>
2020-06-21 15:30:53 +0300

    show some variables

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,11 +2,11 @@ export const environment = {
   production: true,
   firebase: {
     apiKey: process.env.FBS_API_KEY,
-    authDomain: process.env.FBS_AUTH_DOMAIN,
+    authDomain: 'coworkers-cc0e8.firebaseapp.com',
     databaseURL: process.env.FBS_DB_URL,
-    projectId: process.env.FBS_PROJECT_ID,
+    projectId: 'coworkers-cc0e8',
     storageBucket: process.env.FBS_STORAGE_BUCKET,
-    messagingSenderId: process.env.FBS_MES_SENDER_ID,
-    appId: process.env.FBS_APP_ID
+    messagingSenderId: '863499174126',
+    appId: '1:863499174126:web:881db15f14928374aff3ff'
   }
 };
