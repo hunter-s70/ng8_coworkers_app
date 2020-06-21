c459d362ae47ef17bd306bbac961c62180b6bc36
hunter-s70 <serg.buhtoyarov@gmail.com>
2020-06-21 19:50:20 +0300

    add string project id

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,7 +4,7 @@ export const environment = {
     apiKey: process.env.FBS_API_KEY,
     authDomain: process.env.FBS_AUTH_DOMAIN,
     databaseURL: process.env.FBS_DB_URL,
-    projectId: process.env.FBS_PROJECT_ID,
+    projectId: 'coworkers-cc0e8',
     storageBucket: process.env.FBS_STORAGE_BUCKET,
     messagingSenderId: process.env.FBS_MES_SENDER_ID,
     appId: process.env.FBS_APP_ID
