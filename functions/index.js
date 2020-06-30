3b6d8dca1e6e600218f687637c78cb282c8b3cc8
hunter-s70 <serg.buhtoyarov@gmail.com>
2020-06-30 23:18:38 +0300

    my first cloud function

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,6 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+const serviceAccount = require('../coworkers-cc0e8-firebase-adminsdk-i16n6-ce7881ca56.json');
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
@@ -6,3 +8,15 @@ const functions = require('firebase-functions');
 // exports.helloWorld = functions.https.onRequest((request, response) => {
 //  response.send("Hello from Firebase!");
 // });
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://coworkers-cc0e8.firebaseio.com"
+});
+
+// http://localhost:5001/coworkers-cc0e8/us-central1/exportSkills
+exports.exportSkills = functions.https.onRequest(async (req, res) => {
+  const querySnapshot = await admin.firestore().collection('skills').get();
+  const skillsList = querySnapshot.docs.map((doc) => doc.data());
+  res.json({skills: skillsList});
+});
