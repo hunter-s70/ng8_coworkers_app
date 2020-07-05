1ea5e743fb21bd8cd6a23d25d796539bddc81ae9
hunter-s70 <serg.buhtoyarov@gmail.com>
2020-07-05 10:46:27 +0300

    update export function

--- functions/index.js
+++ functions/index.js
@@ -2,8 +2,8 @@ const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 const serviceAccount = require('../coworkers-cc0e8-firebase-adminsdk-i16n6-ce7881ca56.json');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
+// Create and Deploy Your First Cloud Functions
+// https://firebase.google.com/docs/functions/write-firebase-functions
 //
 // exports.helloWorld = functions.https.onRequest((request, response) => {
 //  response.send("Hello from Firebase!");
@@ -14,9 +14,13 @@ admin.initializeApp({
   databaseURL: "https://coworkers-cc0e8.firebaseio.com"
 });
 
-// http://localhost:5001/coworkers-cc0e8/us-central1/exportSkills
+// http://localhost:5001/coworkers-cc0e8/us-central1/exportSkills?skills=true&positions=true
 exports.exportSkills = functions.https.onRequest(async (req, res) => {
-  const querySnapshot = await admin.firestore().collection('skills').get();
-  const skillsList = querySnapshot.docs.map((doc) => doc.data());
-  res.json({skills: skillsList});
+  const collectionsList = Object.keys(req.query);
+  const exportedData = {};
+  for (let i = 0; i < collectionsList.length; i++) {
+    const querySnapshot = await admin.firestore().collection(collectionsList[i]).get();
+    exportedData[collectionsList[i]] = querySnapshot.docs.map((doc) => doc.data());
+  }
+  res.json(exportedData);
 });
