36d5b653153cf36cbb2d04144d29cb55a6bc45ff
hunter-s70 <serg.buhtoyarov@gmail.com>
2020-08-22 12:52:59 +0300

    add project service

--- src/app/pages/add-project/add-project.component.ts
+++ src/app/pages/add-project/add-project.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Project } from '../../classes/project';
+import { ProjectService } from '../../services/project.service';
 
 @Component({
   selector: 'app-add-project',
@@ -7,7 +10,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddProjectComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private prs: ProjectService,
+    public router: Router,
+  ) { }
+
+  project: Project = new Project();
+
+  projectSave(data: object): void {
+    this.prs.addProject({data}).then(() => {
+      this.router.navigate(['app/home']);
+    });
+  }
 
   ngOnInit() {
   }
