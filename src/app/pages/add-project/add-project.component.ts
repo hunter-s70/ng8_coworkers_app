d32b2db7cc4177ac9c004aec31461801a1f9dca3
hunter-s70 <serg.buhtoyarov@gmail.com>
2020-08-24 22:26:57 +0300

    replace employees list request

--- src/app/pages/add-project/add-project.component.ts
+++ src/app/pages/add-project/add-project.component.ts
@@ -1,21 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Router } from '@angular/router';
+import { Subscription } from 'rxjs';
 import { Project } from '../../classes/project';
 import { ProjectService } from '../../services/project.service';
+import { EmployeeService } from '../../services/employee.service';
 
 @Component({
   selector: 'app-add-project',
   templateUrl: './add-project.component.html',
   styleUrls: ['./add-project.component.css']
 })
-export class AddProjectComponent implements OnInit {
+export class AddProjectComponent implements OnInit, OnDestroy {
 
   constructor(
+    private ems: EmployeeService,
     private prs: ProjectService,
     public router: Router,
   ) { }
 
   project: Project = new Project();
+  employeesData: Subscription;
 
   projectSave(data: object): void {
     this.prs.addProject({data}).then(() => {
@@ -23,7 +27,16 @@ export class AddProjectComponent implements OnInit {
     });
   }
 
+  get employeesList() {
+    return this.ems.getEmployeesSelectorList() || [];
+  }
+
   ngOnInit() {
+    this.employeesData = this.ems.getEmployeesListForSelect();
+  }
+
+  ngOnDestroy() {
+    this.employeesData.unsubscribe();
   }
 
 }
--- src/app/pages/edit-project/edit-project.component.ts
+++ src/app/pages/edit-project/edit-project.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
 import { Project } from '../../classes/project';
 import { ProjectService } from '../../services/project.service';
-import { Subscription } from 'rxjs';
+import { EmployeeService } from '../../services/employee.service';
 
 @Component({
   selector: 'app-edit-project',
@@ -12,6 +13,7 @@ import { Subscription } from 'rxjs';
 export class EditProjectComponent implements OnInit, OnDestroy {
 
   constructor(
+    private ems: EmployeeService,
     private prs: ProjectService,
     private route: ActivatedRoute,
     public router: Router,
@@ -21,6 +23,11 @@ export class EditProjectComponent implements OnInit, OnDestroy {
   projectId: string;
   projectData: Subscription;
   projectExists = true;
+  employeesData: Subscription;
+
+  get employeesList() {
+    return this.ems.getEmployeesSelectorList() || [];
+  }
 
   private _getProjectData(projectId: string): void {
     this.projectData = this.prs.getProjectById(projectId).subscribe((data) => {
@@ -37,6 +44,7 @@ export class EditProjectComponent implements OnInit, OnDestroy {
   }
 
   ngOnInit() {
+    this.employeesData = this.ems.getEmployeesListForSelect();
     this.route.params.subscribe(params => {
       this.projectId = params.id;
       this._getProjectData(params.id);
@@ -45,6 +53,7 @@ export class EditProjectComponent implements OnInit, OnDestroy {
 
   ngOnDestroy() {
     this.projectData.unsubscribe();
+    this.employeesData.unsubscribe();
   }
 
 }
